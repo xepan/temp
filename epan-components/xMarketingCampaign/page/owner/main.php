921a83c0d1e30f13c974dd07ee8d29e80a569b70
sinharakesh <rksinha.btech@gmail.com>
2014-12-09 02:16:49 +0530

    xmenus updated to atk4.3

--- epan-components/xMarketingCampaign/page/owner/main.php
+++ epan-components/xMarketingCampaign/page/owner/main.php
@@ -17,20 +17,6 @@ class page_xMarketingCampaign_page_owner_main extends page_componentBase_page_ow
 		$xmrkt_cmg_m->addItem(array('Configurations','icon'=>'gauge-1'),'xMarketingCampaign_page_owner_config');
 
 	}
-
-	// function initMainPage(){
-		
-	// 	$tabs = $this->add('Tabs');
-	// 	$tabs->addTabUrl('xMarketingCampaign/page_owner_dashboard','<i class="fa fa-dashboard"></i> Dashboard');
-	// 	$tabs->addTabUrl('xMarketingCampaign/page_owner_emailcontacts','<i class="fa fa-users"></i> Manage Contacts');
-	// 	$tabs->addTabUrl('xMarketingCampaign/page_owner_newsletters','<i class="fa fa-envelope"></i> Manage NewsLetters');
-	// 	$tabs->addTabUrl('xMarketingCampaign/page_owner_socialcontents','<i class="fa fa-share-alt-square"></i> Add SocialContent');
-	// 	$tabs->addTabUrl('xMarketingCampaign/page_owner_campaigns','<i class="fa fa-calendar-o"></i> Campaigns');
-	// 	$tabs->addTabUrl('xMarketingCampaign/page_owner_scheduledjobs','<i class="fa fa-tasks"></i> Scheduled Jobs');
-	// 	$tabs->addTabUrl('xMarketingCampaign/page_owner_config','<i class="fa fa-cogs"></i> Configurations');
-
-	// }
-
  
 	function page_config(){
 		$this->add('H1')->set('Default Config Page');
--- epan-components/xMenus/page/owner/main.php
+++ epan-components/xMenus/page/owner/main.php
@@ -3,9 +3,11 @@
 class page_xMenus_page_owner_main extends page_componentBase_page_owner_main {
 	function initMainPage(){
 
-		$this->h1->setHTML('<i class="fa fa-navicon"></i> '.$this->component_name. '<small>Different kinds of Responsive and Custom Menus</small>');
-		$this->add('H3')->setHTML('<small> no option At backend</small>');
+		$this->app->layout->template->trySetHTML('page_title','<i class="fa fa-navicon"></i> '.$this->component_name. '<small>Different kinds of Responsive and Custom Menus</small>');
+		$this->app->layout->add('H3')->setHTML('<small> no option At backend</small>');
 
+		$xmenu_m=$this->app->top_menu->addMenu($this->component_name);
+		$xmenu_m->addItem(array('Dashboard','icon'=>'gauge-1'),'xMenus_page_owner_dashboard');
 	}
 
 
