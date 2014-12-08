0126645010d23b9870722658b9bd434c29efa95f
sinharakesh <rksinha.btech@gmail.com>
2014-12-09 01:04:38 +0530

    xImage Gallery Updated to atk4.3

--- epan-components/extendedImages/page/owner/main.php
+++ epan-components/extendedImages/page/owner/main.php
@@ -2,6 +2,7 @@
 
 class page_extendedImages_page_owner_main extends page_componentBase_page_owner_main {
 	function initMainPage(){
+		
 		$this->app->layout->template->trySetHTML('page_title','<i class="fa fa-picture-o"></i> '.$this->component_name.'<small> Images with magics</small>');		
 		$this->app->layout->add('H3')->setHTML('<small>No Options At back end :)</small>');
 		
