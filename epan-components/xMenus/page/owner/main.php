27178d41f19984d9fc8edb09cf66ade3ded03beb
sinharakesh <rksinha.btech@gmail.com>
2014-12-09 02:17:50 +0530

    xmenus updated to 4.3

--- epan-components/xMenus/page/owner/main.php
+++ epan-components/xMenus/page/owner/main.php
@@ -1,8 +1,9 @@
 <?php
 
 class page_xMenus_page_owner_main extends page_componentBase_page_owner_main {
-	function initMainPage(){
-
+	function init(){
+		parent::init();
+		
 		$this->app->layout->template->trySetHTML('page_title','<i class="fa fa-navicon"></i> '.$this->component_name. '<small>Different kinds of Responsive and Custom Menus</small>');
 		$this->app->layout->add('H3')->setHTML('<small> no option At backend</small>');
 
