30eb937389dd6dac3da4ff6d8863a38de8e269b0
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-12-19 12:03:02 +0530

    xshop_application_id method added in api on runtime

--- epan-components/xShop/page/owner/main.php
+++ epan-components/xShop/page/owner/main.php
@@ -4,7 +4,8 @@ class page_xShop_page_owner_main extends page_componentBase_page_owner_main {
 	function init(){
 		parent::init();
 
-		$this->app->layout->template->trySetHTML('page_title','<i class="fa fa-shopping-cart"></i> '.$this->component_name. '<small>Used as ( <i class="fa fa-list"></i> ) Product Listing , Blogs and ( <i class="fa fa-shopping-cart"></i> ) E-commerce kinds of Application</small>');
+		$this->app->layout->add('xShop/View_ApplicationSelector',null,'page_title')->setTitle('<i class="fa fa-shopping-cart"></i> '.$this->component_name. '<small>Used as ( <i class="fa fa-list"></i> ) Product Listing , Blogs and ( <i class="fa fa-shopping-cart"></i> ) E-commerce kinds of Application</small>');
+		// $this->app->layout->template->trySetHTML('page_title');
 			
 		$xshop_m = $this->app->top_menu->addMenu($this->component_name);
 		$xshop_m->addItem(array('Dashboard','icon'=>'gauge-1'),'xShop_page_owner_dashboard');
@@ -19,30 +20,9 @@ class page_xShop_page_owner_main extends page_componentBase_page_owner_main {
 		$xshop_m->addItem(array('AddBlock','icon'=>'gauge-1'),'xShop_page_owner_addblock');
 		$xshop_m->addItem(array('Configuration','icon'=>'gauge-1'),'xShop_page_owner_configuration');
 
-
-		// $cart['item_id']=1;
-		// $cart['qty']=20;
-		// $cart['rate']=12345;
-		// $cart->save(1);
-
-		// $cart =$this->add('xShop/Model_Cart');
-		// $cart['item_id']=1;
-		// $cart['qty']=200;
-		// $cart['rate']=45678;
-		// $cart->save(2);
-
-		// $cart->tryLoad(1);
-		// $this->add('Text')->set($cart['item_id']);
-
-		// foreach ($cart as $junk) {
-		// 	$cart->delete();
-		// }
-
-
-		// $cart =$this->add('xShop/Model_Cart');
-		// $g = $this->add('Grid');
-		// $g->setModel($cart);
-		// $g->controller->importField('id');
+		$this->api->addMethod('xshop_application_id',function($page){
+			return $page->api->memorize('xshop_application_id',$page->api->recall('xshop_application_id',$page->add('xShop/Model_Application')->tryLoadAny()->get('id')));
+		});
 
 	}
 
